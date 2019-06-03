8a1e710ffc97ff3908139b1378702b8b74086a63
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-03 15:21:43 -0400

    turning decrement into an arrow function

--- Assignment5/src/Decrement.js
+++ Assignment5/src/Decrement.js
@@ -3,19 +3,18 @@ import React, {Component} from 'react';
 class Decrement extends Component{
     constructor(props){
         super(props);
-        this.decrement = this.decrement.bind(this);
         this.state = {
             startNumber: this.props.startNumber,
         }
     };
 
-    decrement(){
+    decrement = () => {
         this.state.startNumber > 0 ? this.setState({startNumber: this.state.startNumber - 1}) : alert("Cannot be less than zero")
     }
 
     render(){
         return(
-            <button onClick={() => this.decrement()}>{this.state.startNumber}</button>
+            <button onClick={this.decrement}>{this.state.startNumber}</button>
         )
     }
 }
